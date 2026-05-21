1c7e778587479acff2a200f6d29e345292de9601
Graf-Lev-V <legendarlev2004@gmail.com>
2026-05-22 01:04:35 +0300

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
   const [error, setError] = useState<Error | null>(null)
   const [menu, setMenu] = useState<string>('')
   const [classes, setClasses] = useState<Classes | null>(null)
+  const [menuActive, setMenuActive] = useState<boolean>(false)
 
   function handleSubmit(e: React.SubmitEvent<HTMLFormElement>): void {
     e.preventDefault() 
@@ -85,28 +86,48 @@ export default function Home() {
     })()
   }, [])
 
-  
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {if (e.target instanceof HTMLElement && e.target.closest('.menu-btn')) return; setMenuActive(false)}
+    document.addEventListener('click', handler)
+    return () => document.removeEventListener('click', handler)
+  }, [])
 
   return (
     <main className="h-screen bg-gray-900 text-white flex flex-col">
         <h1 className="w-fit mx-auto">AI Chat</h1>
         <div className="flex-1 overflow-y-auto">
-          {messages.map((message) => 
-            <div key={message.id}>
-              <p className='mb-1'>
-                {message.author}: {message.text} 
-                {message.author === 'User' && <button onClick={() => setMenu(message.id)}>...</button>}
-              </p>
-              {menu === message.id && 
-                <ul>
-                  {classes && Object.entries(classes).map(([key, value]) => <li key={key} onClick={() => {
-                    fetch('http://localhost:8000/add', {
-                      method: 'POST',
-                      headers: {'Content-Type': 'application/json'},
-                      body: JSON.stringify({text: message.text, label: key})
-                    })
-                  }}>{value}</li>)}
-                </ul>}
+          {messages.map((message) =>
+            <div key={message.id} className='flex gap-1 group w-fit'>
+              <p className='mb-1'>{message.author}: {message.text}</p>
+              <span className='relative invisible group-hover:visible' onMouseLeave={() => setMenuActive(false)}>
+                {message.author === 'User' && 
+                <button 
+                  onClick={() => 
+                    { setMenu(message.id); setMenuActive(!menuActive) }} 
+                  className='menu-btn hover: cursor-pointer border px-1 rounded-lg text-sm hidden group-hover:block'
+                >
+                  learn
+                </button>}
+                {menu === message.id && menuActive && 
+                  <ul className='absolute bg-gray-600 z-1 top-full menu-btn'>
+                    {classes && Object.entries(classes).reverse().map(([key, value]) => 
+                    <li 
+                      key={key} 
+                      onClick={() => {
+                        fetch('http://localhost:8000/add', {
+                          method: 'POST',
+                          headers: {'Content-Type': 'application/json'},
+                          body: JSON.stringify({text: message.text, label: key})
+                        })
+                      }}
+                      className='hover:cursor-pointer w-max text-sm'
+                    >
+                      {key}: {value}
+                    </li>
+                    )}
+                  </ul>
+                }
+              </span>
             </div>
           )}
           {error && <p>{error.message}</p>}
